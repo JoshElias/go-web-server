57f16834d32d3f9160fc6b397d96ce7da0b595a0
Josh Elias <josh@daemontech.tools>
2024-07-29 09:21:07 -0400

    feat: convert to file server

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 - Not Found"))
-	})
+	mux.Handle("/", http.FileServer(http.Dir("./")))
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.WriteHeader(http.StatusNotFound)
+	// 	w.Write([]byte("404 - Not Found"))
+	// })
 	fmt.Println("server listening on localhost:8080")
 	if err := http.ListenAndServe("localhost:8080", mux); err == nil {
 		fmt.Println("error starting server")
