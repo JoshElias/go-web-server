96d5370ac5ae820a0fffb52f74b96b4aa91994de
Josh Elias <josh@daemontech.tools>
2024-07-29 14:45:09 -0400

    feat: serve index.html by default

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.WriteHeader(http.StatusNotFound)
-	// 	w.Write([]byte("404 - Not Found"))
-	// })
+	// mux.Handle("/", http.FileServer(http.Dir("./index.html")))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./index.html")
+		// w.WriteHeader(http.StatusNotFound)
+		// w.Write([]byte("404 - Not Found"))
+	})
 	fmt.Println("server listening on localhost:8080")
 	if err := http.ListenAndServe("localhost:8080", mux); err == nil {
 		fmt.Println("error starting server")
