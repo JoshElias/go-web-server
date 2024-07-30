4c83a8127c18e3ed41a20de1b59a676089845d31
Josh Elias <josh@daemontech.tools>
2024-07-30 12:30:18 -0400

    feat: add health endpoint and move assets to public dir

--- main.go
+++ main.go
@@ -8,19 +8,29 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	// mux.Handle("/", http.FileServer(http.Dir("./index.html")))
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./index.html")
-		// w.WriteHeader(http.StatusNotFound)
-		// w.Write([]byte("404 - Not Found"))
-	})
-	mux.HandleFunc("/assets/logo.png", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./public/2CofkLc.png")
-		// w.WriteHeader(http.StatusNotFound)
-		// w.Write([]byte("404 - Not Found"))
+	// mux.HandleFunc("/app/*", func(w http.ResponseWriter, r *http.Request) {
+	// 	r.URL.Path
+	// 	http.ServeFile(w, r, "./index.html")
+	// w.WriteHeader(http.StatusNotFound)
+	// w.Write([]byte("404 - Not Found"))
+	// })
+	// mux.HandleFunc("/assets/logo.png", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, "./public/2CofkLc.png")
+	// 	// w.WriteHeader(http.StatusNotFound)
+	// w.Write([]byte("404 - Not Found"))
+	// })
+	fileServer := http.FileServer(http.Dir("./public"))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	// http.Handle("/", fileServer)
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
 	})
 
 	fmt.Println("server listening on localhost:8080")
 	if err := http.ListenAndServe("localhost:8080", mux); err == nil {
 		fmt.Println("error starting server")
 	}
+
 }
